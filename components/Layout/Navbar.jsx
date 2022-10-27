2098ffc617af97630e32e07b2ce29b9e2174197b
kevicebryan <kevinbryanreligion@gmail.com>
2022-10-27 10:31:54 +0700

    fix about img, navbar img

--- components/Layout/Navbar.jsx
+++ components/Layout/Navbar.jsx
@@ -1,7 +1,6 @@
 import classes from "./Navbar.module.css";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import Image from "next/image";
 
 const Navbar = () => {
   const router = useRouter();
@@ -15,14 +14,7 @@ const Navbar = () => {
       <Link href="/">
         <a className={classes.navitem}>Kevin Bryan</a>
       </Link>
-      <Image
-        src="/static/assets/k.png"
-        alt="k-logo"
-        onClick={logoClickHandler}
-        width={29}
-        height={40}
-        priority
-      />
+      <img src="/static/assets/k.png" alt="k-logo" onClick={logoClickHandler} />
       <div className={classes.right}>
         <Link href="/play">
           <a className={classes.navitem}>Play</a>
