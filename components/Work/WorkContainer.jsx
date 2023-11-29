4281cbcc18f65a9c6531a0c1b4abe5bc3638a776
kevicebryan <kevinbryanreligion@gmail.com>
2023-11-30 01:44:41 +0700

    fix: typos

--- components/Work/WorkContainer.jsx
+++ components/Work/WorkContainer.jsx
@@ -7,10 +7,10 @@ const WORK_LIST = [
     year: "Sep - Nov 2023",
     image: "static/assets/work_images/missionA1.png",
     bodyTop:
-      "An open world exploration game, where player not only explore the world, but also explore various mechanics, as the game contains 8 different mini games, each with its own different mechanic.",
+      "An open world exploration game, where player not only explore the world, but also explore various mechanics.\nThe game contains 8 different mini games, each with its own set of rules.",
     bodyBottom:
-      "A 2.5 month project, where I workd as the tech lead working with 2 other engineers, 2 designers and a project manager. Responisble in making sure the code is clean and maintainable, mergable, and hands down worked on 4 of the 8 minigames avaialble in the game. Also worked on notable mechanics such as, NPC AI movement, First Person Player Movement, and more.",
-    stack: ["Unity", "MacOS"],
+      "A 2.5 month project, where I worked as the Tech Lead working with 2 other engineers, 2 designers & a project manager.\nResponisble in making sure the code is clean & maintainable, fix conflicts, & h&s down worked on 4 of the 8 minigames avaialble in the game.\nAlso worked on notable mechanics such as, NPC AI movement, First Person Player Movement, & more.",
+    stack: ["Unity", "MacOS", "C#"],
     url: "#",
     id: 9,
   },
@@ -19,9 +19,9 @@ const WORK_LIST = [
     year: "2023",
     image: "static/assets/work_images/pikolo.png",
     bodyTop:
-      "Pikolo is dedicated for young adults who want to keep in track of their spending, especially to avoid overspending. Cash flow is divided into 4 flows in this app, Spending, Income, Lending, and Borrow.",
+      "Pikolo is dedicated for young adults who want to keep in track of their spending, especially to avoid overspending.\nCash flow is divided into 4 flows in this app, Spending, Income, Lending, & Borrow.",
     bodyBottom:
-      "A personally made native iOS application, designed and developed by me from the ground up. This app was created using my user's story, needs and demand for a cash flow manager app with good UI and features I need, especially as a student who gets allowance, to make sure my finance is tracked.",
+      "Designed & developed by me from the ground up.\nThis app was created using my user's story, needs & dem& for a cash flow manager app with good UI & features I need,\nespecially as a student who gets allowance, to make sure my finance is tracked.",
     stack: ["SwiftUI", "CoreData", "Figma"],
     url: "https://apps.apple.com/id/app/pikolo-cash-flow-manager/id6463812788",
     id: 8,
@@ -31,9 +31,9 @@ const WORK_LIST = [
     year: "Aug 2023",
     image: "static/assets/work_images/my-rail-travel.gif",
     bodyTop:
-      "A native iOS application designed for new MRT commuters or picnicers to explore destinations around MRT stations in Indonesia. The app also has an Augmented Reality (AR) feature, where user can scan Exit Boards, and destinations in front of those exit will be shown.",
+      "A native iOS application designed for new MRT commuters or picnicers to explore destinations around MRT stations in Indonesia. The app also has an Augmented Reality (AR) feature, where user can scan Exit Boards, & destinations in front of those exit will be shown.",
     bodyBottom:
-      "Worked on this project, as the Tech Lead, where I managed the workload of 2 other engineers, designed the model architecture, and implemented MVVM. Also worked hands down creating the AR feature.",
+      "Worked as the Tech Lead, managing the my personal workload & 2 other engineers,\ndesigned the model architecture, & implemented MVVM.\nResponsible in developing the AR feature.",
     stack: ["SwiftUI", "RealityKit", "CoreLocation"],
     url: "#",
     id: 7,
@@ -43,10 +43,10 @@ const WORK_LIST = [
     year: "Jun - Jul 2023",
     image: "static/assets/work_images/lug-n-loaded.png",
     bodyTop:
-      "A co-op challenge multiplayer game where players act as robbers who need to arrange and fit all their items in a luggage while getting distracted with various obstructions.",
+      "A co-op challenge multiplayer game where players act as robbers who need to arrange & fit all their items in a luggage while getting distracted with various obstructions.",
     bodyBottom:
-      "Worked on the main mechanic, the drag n dropping items, logic of items fitting in the luggage and its boundary logic, & also the game win condition. Also worked on the multithreaading, and implementation of GameKit for the quick match feature. Worked as a Tech Lead and Project Manager on this project using Jira and experience hands down experience on project management and SCRUM workflow.",
-    stack: ["SpriteKit", "GameKit", "Jira"],
+      "Worked on the main mechanic, the drag n dropping items, logic of items fitting in the luggage & its boundary logic, & the game win condition. \nAlso worked on  multithreaading, & implementation of GameKit for the quick match feature. \nWorked as a Tech Lead & Project Manager on this project using Jira implementing SCRUM workflow.",
+    stack: ["SpriteKit", "GameKit", "SwiftUI"],
     url: "https://drive.google.com/file/d/1eSCJ6wbN6RkXBK3X1TjY1bfG3UpAhjTv/view?usp=drive_link",
     id: 6,
   },
@@ -57,7 +57,7 @@ const WORK_LIST = [
     bodyTop:
       "An app that offers families an opportunity to get recommendations on activity to spend their quality time based on their mutual interest. ",
     bodyBottom:
-      "Worked as the Tech Lead, implementing clean code architecture for the team and also applying MVVM. Worked on Core Data for local storing of data, and also created the algorithm for the recommendation page, using weights system to show what best matches the user's interest.",
+      "Worked as the Tech Lead, implementing clean code architecture for the team & also applying MVVM.\nWorked on Core Data for local storage, & also created the algorithm for the recommendation page.",
     stack: ["SwiftUI", "CoreData"],
     url: "https://apps.apple.com/id/app/flanner/id6463742782",
     id: 5,
@@ -67,10 +67,10 @@ const WORK_LIST = [
     year: "2022",
     image: "static/assets/work_images/w3b-scholar.png",
     bodyTop:
-      "A blog website, where users can share articles to help educate Indonesian about Web3, NFT, and the Metaverse in hope to build and develop more Web3 Developers in Indonesia.",
+      "A blog website, where users can share articles to help educate Indonesian about Web3, NFT, & the Metaverse in hope to build & develop more Web3 Developers in Indonesia.",
     bodyBottom:
-      "I build this site together with my team during Informatics Festival 2022, an annual web building competition. My role on this project was mostly on the front end, building the article page, and its functionality such as pagination and searching.",
-    stack: ["NextJS", "CSS Style Component", "Firebase"],
+      "I build this site together with my team during Informatics Festival 2022, an annual web building competition. My role on this project was mostly on the front end, building the article page, & its functionality such as pagination & searching.",
+    stack: ["NextJS", "Vanilla CSS", "Firebase"],
     url: "https://w3b-scholar.vercel.app/",
     id: 4,
   },
@@ -90,9 +90,9 @@ const WORK_LIST = [
     year: "2022",
     image: "static/assets/work_images/forkify.png",
     bodyTop:
-      "A recipe app where you can browse over 1000 recipes. You can view and search through existing recipe or choose to upload your own recipe! It also has a bookmarking feature, to keep track of your favorite recipe.",
+      "A recipe app where you can browse over 1000 recipes. You can view & search through existing recipe or choose to upload your own recipe! It also has a bookmarking feature, to keep track of your favorite recipe.",
     bodyBottom:
-      "This project was the first project where I dealt with pagination, search queries, and actually fetching and manipulating data from an API.",
+      "This project was the first project where I dealt with pagination, search queries, & actually fetching & manipulating data from an API.",
     stack: ["AJAX Javascript"],
     url: "https://kevinbryan-forkify.netlify.app/",
     id: 2,
@@ -102,9 +102,9 @@ const WORK_LIST = [
     year: "2021 - 22",
     image: "static/assets/work_images/filetechno.png",
     bodyTop:
-      "A social media project, currently educating over 4000+ followers on programming and UI/UX design information.",
+      "A social media project, currently educating over 4000+ followers on programming & UI/UX design information.",
     bodyBottom:
-      "Filetechno is created to help educate and share tech-design related information, in a simpler, fun and easier way to understand.",
+      "Filetechno is created to help educate & share tech-design related information, in a simpler, fun & easier way to underst&.",
     stack: ["Figma", "Instagram"],
     url: "https://www.instagram.com/filetechno/",
     id: 1,
