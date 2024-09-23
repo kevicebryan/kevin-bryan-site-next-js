c5da784bc47e3ac946b0924fc5092e6caf2c1b36
kevicebryan <kevinbryanreligion@gmail.com>
2024-09-23 12:30:29 +0700

    style: add gradient, hide polaroid on mobile

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./Navbar";
 import Head from "next/head";
 import classes from "./Layout.module.css";
 import Footer from "./Footer";
+import Image from "next/image";
 
 const Layout = (props) => {
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const topGradient = document.querySelector(`.${classes.gradient_top}`);
+      const bottomGradient = document.querySelector(`.${classes.gradient_btm}`);
+      if (topGradient && bottomGradient) {
+        topGradient.style.transform = `translateY(${scrollY * 0.25}px)`;
+        bottomGradient.style.transform = `translateY(${scrollY * -0.25}px)`;
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll); // Cleanup event listener on unmount
+    };
+  }, []); // Empty dependency array to run once on mount
+
   return (
     <div className={classes.container}>
       <Head>
@@ -28,6 +47,21 @@ const Layout = (props) => {
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="Kevin Bryan" />
       </Head>
+      <Image
+        src="/static/assets/gradient.svg"
+        alt="gradient"
+        width={760}
+        height={820}
+        className={classes.gradient_top}
+      />
+      <Image
+        src="/static/assets/gradient.svg"
+        alt="gradient"
+        width={760}
+        height={820}
+        className={classes.gradient_btm}
+      />
+
       <Navbar />
       {props.children}
       <Footer />
